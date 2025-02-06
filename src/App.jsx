8b52e5338b77b66f647ff3e6da2d8ddb7a1d92ef
Gerges-GN <gerges7gn1@gmail.com>
2025-02-06 18:29:30 +0200

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <div  className="font-thin px-10 pb-20">
+        <h1 className="text-5xl mb-2">Empty Project!</h1>
+        <p >Copy, Rename and GO</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
